6acaa93833f7d1ca41107648c6d6427fcd5e138a
jernst766 <42757012+jernst766@users.noreply.github.com>
2019-01-17 10:47:50 -0600

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Justin Ernst
+ * Date: 2016/11/02
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Justin Ernst\n");
+
+  return 0;
+}
